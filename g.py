0dc22847252372878985c3423fcd36568f0ce3a7
DoVri <137869213+DoVri@users.noreply.github.com>
2023-07-17 18:19:14 +0700

    Update g.py

--- g.py
+++ g.py
@@ -161,9 +161,9 @@ def spoofer():
     addr[1] = str(random.randrange(0, 255))
     addr[2] = str(random.randrange(0, 255))
     addr[3] = str(random.randrange(2, 254))
-    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
-    return assemebled
-       
+    assembled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
+    return assembled
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Tanggal kedaluwarsa (misalnya 7 hari setelah saat ini)
